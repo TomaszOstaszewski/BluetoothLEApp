12cb961e9f50ff8d13dc7101315c7b500662385a
TomaszOstaszewski <tomasz.ostaszewski@interia.pl>
2014-07-24 23:08:34 +0200

    Refactoring
    Minor refactoring of MainActivity

--- src/com/example/myanotherapp/MainActivity.java
+++ src/com/example/myanotherapp/MainActivity.java
@@ -15,17 +15,24 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+/**
+ * 
+ * @author Tomek
+ *
+ */
 public class MainActivity extends Activity
 implements BluetoothAdapter.LeScanCallback {
 
+	/** 
+	 * 
+	 * @author 
+	 *
+	 */
 	public enum SCAN_STATUS {
-		STANDBY,
-		SCANNING
+		STANDBY, //< In standby 
+		SCANNING //< Doing scanning
 	}
 	public final static String EXTRA_MESSAGE = "com.example.myanotherapp.EXTRA_MESSAGE";
-
-	//private SparseArray<BluetoothDevice> mDevices;
-	private SCAN_STATUS mScanStatus = SCAN_STATUS.STANDBY;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,30 +44,23 @@ implements BluetoothAdapter.LeScanCallback {
 		mRSSIControl = (EditText)findViewById(R.id.editText1);
 		mAnnouncedName = (EditText)findViewById(R.id.editText3);
 		mScanOnOffSwitch = (Switch)findViewById(R.id.switch1);
-		
-		if (null == mScanOnOffSwitch)
-		{
+
+		if (null == mScanOnOffSwitch) {
 			Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
 			finish();
 			return;
 		}
+		// Install handler for switch toggle
 		mScanOnOffSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				// TODO Auto-generated method stub
 				MainActivity mainAct = (MainActivity)buttonView.getContext();
-				System.out.println(mainAct.toString());
-				if (isChecked)
-				{
-					mBluetoothAdapter.startLeScan(mainAct);
-					mScanStatus = SCAN_STATUS.SCANNING;
+				if (isChecked)	{
+					mainAct.startScanning();
 				}
-				else
-				{
-					mBluetoothAdapter.stopLeScan(mainAct);
-					mScanStatus = SCAN_STATUS.STANDBY;
+				else {
+					mainAct.stopScanning();
 				}
-				System.out.println("status: "+ isChecked);
 			}
 		});
 	}
@@ -70,6 +70,12 @@ implements BluetoothAdapter.LeScanCallback {
 		super.onResume();
 		mScanOnOffSwitch.setActivated(false);
 
+		if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) 	{
+			Toast.makeText(this,  "No LE support", Toast.LENGTH_SHORT).show();
+			finish();
+			return;
+		}
+
 		if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
 			//Bluetooth is disabled
 			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -78,35 +84,23 @@ implements BluetoothAdapter.LeScanCallback {
 			return;
 		}
 		mScanOnOffSwitch.setActivated(false);
-
-		if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE))
-		{
-			Toast.makeText(this,  "No LE support", Toast.LENGTH_SHORT).show();
-			finish();
-			return;
-		}
-
 	}
 
+	/**
+	 * @brief
+	 */
 	protected void onPause() {
 		super.onPause();
-		switch (mScanStatus) {
-		case SCANNING:
-			mBluetoothAdapter.stopLeScan(this);
-			break;
-		default:
-			break;
-		}
+		stopScanning();
 	}
 
-	public void sendMessage(View aView)
-	{
-		mBluetoothAdapter.startLeScan(this);
-		mScanStatus = SCAN_STATUS.SCANNING;
-	}
-
-	public void onLeScan(BluetoothDevice aDevice, int rssi, byte[] scanRecord)
-	{
+	/**
+	 * @brief
+	 * @param
+	 * @param
+	 * @param
+	 */
+	public void onLeScan(BluetoothDevice aDevice, int rssi, byte[] scanRecord)	{
 		final byte[] scanRecordCopy = scanRecord;
 		final int rssiCopy = rssi;
 		this.runOnUiThread(new Runnable() {
@@ -115,15 +109,52 @@ implements BluetoothAdapter.LeScanCallback {
 				AdvIndData anEntry = new AdvIndData();
 				BluetoothLEAdvIndParser.parseAdvInd(scanRecordCopy, anEntry);
 				mRSSIControl.setText(Integer.toString(rssiCopy));
-				System.out.println(anEntry.getAnnouncedName());
 				mAnnouncedName.setText(anEntry.getAnnouncedName());
 			}
 		});
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	private boolean startScanning()	{
+		boolean bRetVal = false;
+		switch (mScanStatus) {
+		case STANDBY:
+			bRetVal = mBluetoothAdapter.startLeScan(this);
+			if (bRetVal) {
+				mScanStatus = SCAN_STATUS.SCANNING;
+			}
+			Log.d(Thread.currentThread().getStackTrace()[0].getMethodName(), mScanStatus.toString());
+			break;
+		case SCANNING:	
+		default:
+			break;
+		}
+		return bRetVal;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private boolean stopScanning()	{
+		switch (mScanStatus) {
+		case SCANNING:
+			mBluetoothAdapter.stopLeScan(this);
+			mScanStatus = SCAN_STATUS.STANDBY;
+			Log.d(Thread.currentThread().getStackTrace()[0].getMethodName(), mScanStatus.toString());
+			return true;
+		case STANDBY:
+		default:
+			return false;
+		}
+	}
+
+	private SCAN_STATUS mScanStatus = SCAN_STATUS.STANDBY;
 	private Switch mScanOnOffSwitch;
 	private EditText mRSSIControl;
 	private EditText mAnnouncedName;
 	private BluetoothAdapter mBluetoothAdapter; 
-	EditText mEditText;
 }
